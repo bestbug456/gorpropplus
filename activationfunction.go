41ad89166c5f42a657b2d5ae5add870eed761f77
bestbug <bestbug@localhost>
2018-08-22 18:47:41 +0200

    add missing comment

--- activationfunction.go
+++ activationfunction.go
@@ -4,18 +4,22 @@ import (
 	"math"
 )
 
+// IperbolicTangent return the tanh value
 func IperbolicTangent(neuronValue float64) float64 {
 	return math.Tanh(neuronValue)
 }
 
+// DerivateIperbolicTangent return the derivate of tanh
 func DerivateIperbolicTangent(neuronValue float64) float64 {
 	return 1.0 - math.Pow(neuronValue, 2.0)
 }
 
+// Logistic is the classic logistic function
 func Logistic(neuronValue float64) float64 {
 	return 1.0 / (1.0 + math.Exp(-neuronValue))
 }
 
+// DerivateLogistic is the derivate of logistic
 func DerivateLogistic(neuronValue float64) float64 {
 	return neuronValue * (1.0 - neuronValue)
 }
